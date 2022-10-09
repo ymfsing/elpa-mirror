5c05f33722fec2d1da3eab2c2d0792e8463a3e4e
ymfsing <ymfsing@outlook.com>
2022-10-09 11:12:11 +0800

    update

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
       '(("melpa" . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/melpa/")
         ("org"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/org/")
         ("gnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/")
-		("nongnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/nongnu/")))
+	("nongnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/nongnu/")))
 
 (package-initialize)
 
@@ -52,105 +52,105 @@
     hl-todo
     speed-type
     flycheck
-	;;; init-file
-	dired-git-info
-	dired-rsync
-	diredfl
-	;;; init-keymaps
-	;; evil
-	;; evil-lion
-	meow
-	keyfreq
-	keycast
-	;;; init-minibuffer
-	vertico
-	marginalia
-	orderless
-	consult
-	consult-flycheck
-	embark
-	;;; init-ui
-	posframe
-	all-the-icons
-	beacon
-	doom-themes
-	doom-modeline
-	diminish
-	rainbow-delimiters
-	;; symbol-overlay
-	burly
-	winum
-	shackle
-	;;; init-vsc
-	;; magit
-	;;; vc-msg
-	blamer
-	diff-hl
-	;;; init-lang
-	;; eglot
-	lsp-mode
-	lsp-pyright
-	lsp-ui
-	;; citre
-	format-all
-	;; BUG can't work when aggressive-indent-mode
-	;; aggressive-indent
-	devdocs-browser
-	;; dap-mode
-	germanium
-	;;; init-applescript
-	applescript-mode
-	;;; init-docker
-	dockerfile-mode
-	docker-compose-mode
-	;;; init-latex
-	;; auctex
-	;;; init-lisp
-	elisp-demos
-	sly
-	suggest
-	;;; init-lua
-	lua-mode
-	;;; init-markdown
-	markdown-mode
-	markdown-toc
-	;;; init-org
-	org
-	org-contrib
-	org-appear
-	org-download
-	iscroll
-	org-transclusion
-	toc-org
-	ox-pandoc
-	anki-editor
-	habitica
-	org-roam
-	;; emacsql-sqlite-builtin ;; for emacs 29
-	org-roam-ui
-	;;; init-ruby
-	ruby-mode
-	;;; init-web
-	web-mode
-	emmet-mode
-	typescript-mode
-	impatient-mode
-	;;; init-serialization
-	json-mode
-	yaml-mode
-	;;; init-blog
-	org-static-blog
-	;;; init-browser
-	;; reddigg
-	hnreader
-	;;; init-draw
-	plantuml-mode
-	osm
-	;;; init-gnus
-	;; nntwitter
-	;; init-leetcode
-	leetcode
-	)
+    ;;; init-file
+    dired-git-info
+    dired-rsync
+    diredfl
+    ;;; init-keymaps
+    ;; evil
+    ;; evil-lion
+    meow
+    keyfreq
+    keycast
+    ;;; init-minibuffer
+    vertico
+    marginalia
+    orderless
+    consult
+    consult-flycheck
+    embark
+    ;;; init-ui
+    posframe
+    all-the-icons
+    beacon
+    doom-themes
+    doom-modeline
+    diminish
+    rainbow-delimiters
+    ;; symbol-overlay
+    burly
+    winum
+    shackle
+    ;;; init-vsc
+    ;; magit
+    ;;; vc-msg
+    blamer
+    diff-hl
+    ;;; init-lang
+    ;; eglot
+    lsp-mode
+    lsp-pyright
+    lsp-ui
+    ;; citre
+    format-all
+    ;; BUG can't work when aggressive-indent-mode
+    ;; aggressive-indent
+    devdocs-browser
+    ;; dap-mode
+    germanium
+    ;;; init-applescript
+    applescript-mode
+    ;;; init-docker
+    dockerfile-mode
+    docker-compose-mode
+    ;;; init-latex
+    ;; auctex
+    ;;; init-lisp
+    elisp-demos
+    sly
+    suggest
+    ;;; init-lua
+    lua-mode
+    ;;; init-markdown
+    markdown-mode
+    markdown-toc
+    ;;; init-org
+    org
+    org-contrib
+    org-appear
+    org-download
+    iscroll
+    org-transclusion
+    toc-org
+    ox-pandoc
+    anki-editor
+    habitica
+    org-roam
+    ;; emacsql-sqlite-builtin ;; for emacs 29
+    org-roam-ui
+    ;;; init-ruby
+    ruby-mode
+    ;;; init-web
+    web-mode
+    emmet-mode
+    typescript-mode
+    impatient-mode
+    ;;; init-serialization
+    json-mode
+    yaml-mode
+    ;;; init-blog
+    org-static-blog
+    ;;; init-browser
+    ;; reddigg
+    hnreader
+    ;;; init-draw
+    plantuml-mode
+    osm
+    ;;; init-gnus
+    ;; nntwitter
+    ;;; init-leetcode
+    leetcode
+    )
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
@@ -186,14 +186,14 @@ Missing packages are installed automatically."
 ;; quelpa packages https://github.com/quelpa/quelpa
 
 (setq quelpa-checkout-melpa-p nil
-	  quelpa-update-melpa-p nil
-	  quelpa-melpa-recipe-stores nil
-	  quelpa-git-clone-depth 1)
+      quelpa-update-melpa-p nil
+      quelpa-melpa-recipe-stores nil
+      quelpa-git-clone-depth 1)
 
 (quelpa '(pyim-tsinghua-dict
-		  :fetcher github
-		  :repo "redguardtoo/pyim-tsinghua-dict"
-		  :files ("*.el" "*.pyim")))
+	  :fetcher github
+	  :repo "redguardtoo/pyim-tsinghua-dict"
+	  :files ("*.el" "*.pyim")))
 
 (quelpa '(cape-yasnippet :fetcher github :repo "elken/cape-yasnippet"))
 
@@ -210,10 +210,10 @@ Missing packages are installed automatically."
 (quelpa '(org-media-note :fetcher github :repo "yuchen-lea/org-media-note"))
 
 (quelpa '(telega
-		  :fetcher github
-		  :repo "zevlg/telega.el"
-		  :branch "release-0.8.0"
-		  :files (:defaults "etc" "server" "contrib" "Makefile")))
+	  :fetcher github
+	  :repo "zevlg/telega.el"
+	  :branch "release-0.8.0"
+	  :files (:defaults "etc" "server" "contrib" "Makefile")))
 
 ;; (quelpa '(ement :fetcher github :repo "alphapapa/ement.el"))
 
@@ -221,11 +221,11 @@ Missing packages are installed automatically."
 ;; some pinned packages
 
 (quelpa '(aggressive-indent
-		  :fetcher github
-		  :repo "Malabarba/aggressive-indent-mode"
-		  :commit "70b3f0add29faff41e480e82930a231d88ee9ca7"
-		  :files ("*.el")))
+	  :fetcher github
+	  :repo "Malabarba/aggressive-indent-mode"
+	  :commit "70b3f0add29faff41e480e82930a231d88ee9ca7"
+	  :files ("*.el")))
 
-		'
+
 
 ;;; init.el ends here
