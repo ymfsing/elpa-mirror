5a1f3eb51f86279e66a0468a6bff77f6dcd6970e
ymfsing <ymfsing@outlook.com>
2022-10-26 12:29:04 +0800

    update package-archives

--- init.el
+++ init.el
@@ -12,10 +12,19 @@
 ;; Initialize package sources
 (require 'package)
 
-(setq package-archives
-      '(("melpa" . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/melpa/")
-        ("org"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/org/")
-        ("gnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/")))
+;; Emacs 27.x has gnu elpa as the default
+;; Emacs 28.x adds the nongnu elpa to the list by default, so only
+;; need to add nongnu when this isn't Emacs 28+
+(when (version< emacs-version "28")
+  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+(add-to-list 'package-archives '("stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(customize-set-variable 'package-archive-priorities
+                        '(("gnu"    . 99)   ; prefer GNU packages
+                          ("nongnu" . 80)   ; use non-gnu packages if not found in GNU elpa
+                          ("stable" . 70)   ; prefer "released" versions from melpa
+                          ("melpa"  . 0)))  ; if all else fails, get it from melpa
 
 (package-initialize)
 (package-refresh-contents)
